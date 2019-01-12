e1d515122b9a764cf9d52040280646622ed8ab5b
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2019-01-11 20:36:54 -0800

    New project

---
+++ app/src/main/java/com/rubyhuntersky/stockcatalog/StockSearchActivity.kt
@@ -0,0 +1,12 @@
+package com.rubyhuntersky.stockcatalog
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class StockSearchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_stock_search)
+    }
+}
