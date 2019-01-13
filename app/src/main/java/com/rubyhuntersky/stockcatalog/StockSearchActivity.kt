2441f2e766e19504c7a3371bdb98b85768b78ac8
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2019-01-13 11:31:17 -0800

    Add SearchView to activity

--- app/src/main/java/com/rubyhuntersky/stockcatalog/StockSearchActivity.kt
+++ app/src/main/java/com/rubyhuntersky/stockcatalog/StockSearchActivity.kt
@@ -1,12 +1,14 @@
 package com.rubyhuntersky.stockcatalog
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_stock_search.*
 
 class StockSearchActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setSupportActionBar(toolbar)
         setContentView(R.layout.activity_stock_search)
     }
 }
